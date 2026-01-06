b5400f519e2fa376e00d6cabbbf40e22a14ac165
Eduardo Santana <dudu_lima.11@hotmail.com>
2026-01-06 14:27:33 -0300

    adjustment base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/abracamente/',
+    base: '/abracamente-landing-page/',
 
     server: {
       port: 3000,
